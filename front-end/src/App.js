5caadadb002dcfa34e818fb8a17cd3f27938f46c
Hogan Luo <hoganluo1028@gmail.com>
2024-01-09 12:19:43 +0000

    09th Jan

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,5 +3,4 @@ import React from 'react'
 const App = () => {
   return <h1>Welcome to Proshop</h1>
 };
-
 export default App;
---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+function Header() {
+  return (
+    <header>
+        <Navbar b
+    </header>
+  )
+}
+
+export default Header
