5525c3a619534f92119a37a9032f4f0f30c05f64
Hogan Luo <hoganluo1028@gmail.com>
2024-01-10 12:13:56 +0000

    10 th Jan

--- front-end/src/components/Product.jsx
+++ front-end/src/components/Product.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { Link, link } from 'react-router-dom'
 
 const Product = ( { product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant="top" />
-        </a>
+        </Link>
 
         <Card.Body>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as="div">
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
             <Card.Text as="h3">
             {/* Card.Text allows you to use any element you want. It can be div or heading or whatever */}
                 ${product.price}
