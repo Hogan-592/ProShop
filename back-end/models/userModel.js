610f5cba319a54fb2b06012cfbbe1080f84d8c32
Hogan Luo <hoganluo1028@gmail.com>
2024-01-12 18:15:01 +0000

    12th Jan

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         require: true,
     },
-    emial: {
+    email: {
         type: String,
         required: true,
         unique: true,
