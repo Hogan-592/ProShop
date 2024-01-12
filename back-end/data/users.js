7d3466d43835e3f8e9697f38a50eb19b1ab2f447
Hogan Luo <hoganluo1028@gmail.com>
2024-01-12 12:32:38 +0000

    12th Jan

---
+++ back-end/data/users.js
@@ -0,0 +1,26 @@
+// cretae dummy users. And bcrypt allows us to hash the password.
+
+import bcrypt from 'bcryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    },
+    {
+        name: 'Jane Doe',
+        email: 'Jane@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    }
+];
+
+export default users;
