476fd5da4d33a90dc0b038e48483ff1adbdcfd63
Hogan Luo <hoganluo1028@gmail.com>
2024-01-21 10:53:36 +0000

    21th Jan

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler} from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 dotenv.config();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
 
@@ -17,6 +18,7 @@ app.get('/', (req, res) => {
 
 //Whenever we go to api/products this route, it is going to productRoutes file.
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
