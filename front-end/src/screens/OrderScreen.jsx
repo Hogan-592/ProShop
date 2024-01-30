227fb34b5635f44a14efc867fdfe996e014f2aac
Hogan Luo <hoganluo1028@gmail.com>
2024-01-30 17:18:05 +0000

    30th Jan

--- front-end/src/screens/OrderScreen.jsx
+++ front-end/src/screens/OrderScreen.jsx
@@ -2,15 +2,46 @@ import { Link, useParams } from 'react-router-dom';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice';
+import { useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClientIdQuery } from '../slices/ordersApiSlice';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
-import React from 'react'
 
 const OrderScreen = () => {
   // Want to get the data from the database by going through the query
   const { id: orderId } = useParams();
 
   const { data: order, refetch, isLoading, error } = useGetOrderDetailsQuery(orderId);
+
+  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+
+  const { data: paypal, isLoading: loadingPayPal, error: errorPayPal } = useGetPayPalClientIdQuery();
+
+  const { userInfo } =  useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
+        const loadPayPalScript = async () => {
+            paypalDispatch({
+                type: 'resetOptions',
+                value: {
+                    'client-id': paypal.clientId,
+                    currency: 'USD',
+                }
+            });
+            paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
+        }
+        if (order && !order.isPaid) {
+            if (!window.paypal) {
+                loadPayPalScript();
+            }
+        }
+    }
+  }, [order, paypal, paypalDispatch, loadingPayPal, errorPayPal]);
 
   return (
     isLoading ? <Loader /> : error ? <Message variant="danger" /> : (
