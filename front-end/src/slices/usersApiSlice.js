fbf974c23768ed4786ae80c054f6368c5870509b
Hogan Luo <hoganluo1028@gmail.com>
2024-02-04 12:56:59 +0000

    4th Feb

--- front-end/src/slices/usersApiSlice.js
+++ front-end/src/slices/usersApiSlice.js
@@ -28,9 +28,23 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             url: `${USERS_URL}/profile`,
             method: 'PUT',
             body: data,
-        })
+        }),
     }),
+    getUsers: builder.query({
+        query: () => ({
+            url: USERS_URL
+        }),
+        providesTags: ['Users'],
+        keepUnusedDataFor: 5
+    }),
+    deleteUser: builder.mutation({
+        query: (userId) => ({
+            url: `${USERS_URL}/${userId}`,
+            method: 'DELETE',
+
+        })
+    })
 }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation } = usersApiSlice;
